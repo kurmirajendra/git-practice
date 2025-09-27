92584831633b5b080e04213b9bc7ca600f02a6d5
kurmirajendra <rajendrakurmi78@gmail.com>
2025-09-27 16:58:51 +0530

    Hello.md

---
+++ loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &num);
+
+    int arr[num];  // Now 'num' is declared before using it
+
+    // Input elements
+    for(int i = 0; i < num; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    // Print elements
+    printf("\nThe elements in the array are:\n");
+    for(int i = 0; i < num; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
